c42dfef92255ce4bdd9717659870f7c4bb0b2c6e
Oguzhan Cakmak <oguzhanmc@gmail.com>
2017-12-19 09:20:35 -0500

    Initial Commit

---
+++ UberCodingChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UberCodingChallenge
+//
+//  Created by muratcakmak on 19.12.2017.
+//  Copyright © 2017 Murat Cakmak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
