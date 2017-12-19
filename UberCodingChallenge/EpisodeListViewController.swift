6ab519cdf5121c3fb372ab886d0fc3622147cd70
Oguzhan Cakmak <oguzhanmc@gmail.com>
2017-12-19 13:01:32 -0500

    install swiftyjson

--- UberCodingChallenge/EpisodeListViewController.swift
+++ UberCodingChallenge/EpisodeListViewController.swift
@@ -19,12 +19,10 @@ class EpisodeListViewController: UITableViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
   }
 }
 
