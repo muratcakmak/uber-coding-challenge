f80131628a365e5a0175f7ef8a69553c9065f574
Oguzhan Cakmak <oguzhanmc@gmail.com>
2017-12-19 16:08:22 -0500

    refactor the project

---
+++ UberCodingChallenge/New Group/EpisodeListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EpisodeListTableViewCell.swift
+//  UberCodingChallenge
+//
+//  Created by muratcakmak on 19.12.2017.
+//  Copyright © 2017 Murat Cakmak. All rights reserved.
+//
+
+import UIKit
+
+class EpisodeListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- UberCodingChallenge/View/EpisodeListViewController.swift
+++ UberCodingChallenge/View/EpisodeListViewController.swift
@@ -23,6 +23,7 @@ class EpisodeListViewController: UITableViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    tableView.rowHeight = UITableViewAutomaticDimension
     downloadEpisodeList()
   }
 
@@ -32,38 +33,47 @@ class EpisodeListViewController: UITableViewController {
 
 }
 
-
 // Table view's delegate functions
 extension EpisodeListViewController {
-  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return self.items.count
-  }
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count
+    }
 
-  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "EpisodeItem", for: indexPath)
-    let item = items[indexPath.row]
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EpisodeItem", for: indexPath)
+        
+        let item = items[indexPath.row]
+        
+        // Access UI elements by tag. Defined on Interface Builder.
+        let thumbnail = cell.viewWithTag(100) as! UIImageView
+        thumbnail.sd_setImage(with: item.imageURL, placeholderImage: UIImage(named: "placeholder.png"))
+
+        let name = cell.viewWithTag(101) as! UILabel
+        name.text = item.name
+
+        let airdate = cell.viewWithTag(102) as! UILabel
+        airdate.text = "Air Date: \(item.airdate)"
+
+        let episode = cell.viewWithTag(103) as! UILabel
+        episode.text = "S\(item.season)E\(item.number)"
+
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
     
-    let thumbnail = cell.viewWithTag(100) as! UIImageView
-    thumbnail.sd_setImage(with: item.imageURL, placeholderImage: UIImage(named: "placeholder.png"))
-    
-    let name = cell.viewWithTag(101) as! UILabel
-    name.text = item.name
-    
-    let airdate = cell.viewWithTag(102) as! UILabel
-    airdate.text = "Air Date: \(item.airdate)"
-    
-    let episode = cell.viewWithTag(103) as! UILabel
-    episode.text = "S\(item.season)E\(item.number)"
-    
-    return cell
-  }
 }
 
 
 
 // Network requests
 extension EpisodeListViewController {
-    
     func downloadEpisodeList() {
         Alamofire.request(
           "https://api.tvmaze.com/singlesearch/shows?q=silicon-valley&embed=episodes",
@@ -87,7 +97,8 @@ extension EpisodeListViewController {
         }
     }
     
-    func parse(json: JSON) {
+    private func parse(json: JSON) {
+        // Access episodes array, fetch attributes, create an EpisodeListItem struct, put it into items array
         for episode in json["_embedded"]["episodes"].arrayValue {
             let id = episode["id"].intValue
             let name = episode["name"].stringValue
@@ -99,6 +110,10 @@ extension EpisodeListViewController {
             let item = EpisodeListItem(id: id, name: name, season: season, number: number, airdate: airdate, imageURL: imageURL)
             self.items.append(item)
         }
+        // Sorts episodes by their ids
+        self.items.sort { $0.id < $1.id }
+        
+        // Update cells
         tableView.reloadData()
     }
 }
